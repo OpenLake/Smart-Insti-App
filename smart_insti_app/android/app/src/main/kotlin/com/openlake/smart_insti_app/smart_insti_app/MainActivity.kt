fa88b1b156aeec99c50724f027eac48df7b72fef
nailsonseat <aadarshv3@gmail.com>
2024-01-17 14:14:10 +0530

    Configured flutter awesome notifications

--- smart_insti_app/android/app/src/main/kotlin/com/openlake/smart_insti_app/smart_insti_app/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("unused")
-
-package com.openlake.smart_insti_app.smart_insti_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-@Suppress("unused")
-class MainActivity: FlutterActivity()
