9af5129cc2e54a7507d20383f5378e9bae120408
nailsonseat <aadarshv3@gmail.com>
2024-01-19 00:16:50 +0530

    fixed frontend branch overlaps

--- smart_insti_app/android/app/src/main/kotlin/com/openlake/smartinstiapp/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("unused")
-
-package com.openlake.smartinstiapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-@Suppress("unused")
-class MainActivity: FlutterActivity()
--- smart_insti_app/lib/provider/user_Provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class UserProvider extends ChangeNotifier{
-
-}
