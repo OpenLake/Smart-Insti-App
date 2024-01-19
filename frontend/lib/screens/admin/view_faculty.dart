730b1cc49619661333aab38875c669841cfc430c
nailsonseat <aadarshv3@gmail.com>
2024-01-19 22:12:13 +0530

    initial view faculty page with search bar

---
+++ frontend/lib/screens/admin/view_faculty.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:responsive_framework/responsive_scaled_box.dart';
+
+class ViewFaculty extends StatelessWidget {
+  const ViewFaculty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveScaledBox(
+      width: 411,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SizedBox(
+                width: 390,
+                child: SearchBar(
+                  hintText: 'Enter faculty name',
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () => context.pop(),
+                  ),
+                  shadowColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+              ),
+            )
+          ],
+        ),
+        body: const Center(
+          child: Text('View faculty'),
+        ),
+      ),
+    );
+    ;
+  }
+}
