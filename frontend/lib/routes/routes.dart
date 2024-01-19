ce355e3b263b27508c2a0c1c12ce6318cb0e80cd
nailsonseat <aadarshv3@gmail.com>
2024-01-19 22:09:03 +0530

    routed admin pages as root

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../screens/home.dart';
+import 'package:smart_insti_app/screens/admin/add_courses.dart';
+import 'package:smart_insti_app/screens/admin/add_students.dart';
+import 'package:smart_insti_app/screens/admin/admin_home.dart';
+import 'package:smart_insti_app/screens/admin/view_students.dart';
+import '../screens/admin/add_faculty.dart';
+import '../screens/admin/view_courses.dart';
+import '../screens/admin/view_faculty.dart';
 
 final GoRouter routes = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      pageBuilder: (context, state) => const MaterialPage(child: Home()),
+      pageBuilder: (context, state) => const MaterialPage(child: AdminHome()),
+      routes: [
+        GoRoute(
+          path: 'add_students',
+          pageBuilder: (context, state) =>  MaterialPage(child: AddStudents()),
+        ),
+        GoRoute(
+          path: 'add_faculty',
+          pageBuilder: (context, state) => MaterialPage(child: AddFaculty()),
+        ),
+        GoRoute(
+          path: 'add_courses',
+          pageBuilder: (context, state) => MaterialPage(child: AddCourses()),
+        ),
+        GoRoute(
+          path:'view_students',
+          pageBuilder: (context, state) => MaterialPage(child: ViewStudents()),
+        ),
+        GoRoute(
+          path:'view_faculty',
+          pageBuilder: (context, state) => MaterialPage(child: ViewFaculty()),),
+        GoRoute(
+          path:'view_courses',
+          pageBuilder: (context, state) => MaterialPage(child: ViewCourses()),
+        )
+      ]
     )
   ],
 );
