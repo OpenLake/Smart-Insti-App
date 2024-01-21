f5d1f7806e633c57a99aa54ededb9ff4b29e1b48
nailsonseat <aadarshv3@gmail.com>
2024-01-21 13:48:36 +0530

    added validators

--- frontend/lib/constants/constants.dart
+++ frontend/lib/constants/constants.dart
@@ -9,3 +9,49 @@ class AppConstants{
 class LayoutConstants{
 
 }
+
+class Validators{
+  static String? emailValidator(String? value){
+    if(value == null || value.isEmpty){
+      return "Email cannot be empty";
+    }
+    if(!EmailValidator.validate(value)){
+      return "Invalid email";
+    }
+    return null;
+  }
+
+  static String? branchValidator(String? value){
+    if(value == null || value.isEmpty){
+      return "Branch cannot be empty";
+    }
+    return null;
+  }
+
+  static String? nameValidator(String? value){
+    if(value == null || value.isEmpty){
+      return "Name cannot be empty";
+    }
+    return null;
+  }
+
+  static String? rollNumberValidator(String? value){
+    if(value == null || value.isEmpty){
+      return "Roll Number cannot be empty";
+    }
+    if(value.contains(r"!@#%^&*()_+{}||:<>?/.,$")){
+      return "Invalid Roll Number";
+    }
+    return null;
+  }
+
+  static String? courseCodeValidator(String? value){
+    if(value == null || value.isEmpty){
+      return "Course Code cannot be empty";
+    }
+    if(value.contains(r"!@#%^&*()_+{}||:<>?/.,$")){
+      return "Invalid Course Code";
+    }
+    return null;
+  }
+}
