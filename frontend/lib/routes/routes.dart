e46f42737729dceeb76ff1cdbdbbb1dc645d7775
nailsonseat <aadarshv3@gmail.com>
2024-01-21 21:55:30 +0530

    added admin routes

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -5,39 +5,41 @@ import 'package:smart_insti_app/screens/admin/add_students.dart';
 import 'package:smart_insti_app/screens/admin/admin_home.dart';
 import 'package:smart_insti_app/screens/admin/view_students.dart';
 import '../screens/admin/add_faculty.dart';
+import '../screens/admin/add_menu.dart';
 import '../screens/admin/view_courses.dart';
 import '../screens/admin/view_faculty.dart';
 
 final GoRouter routes = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      pageBuilder: (context, state) => const MaterialPage(child: AdminHome()),
-      routes: [
-        GoRoute(
-          path: 'add_students',
-          pageBuilder: (context, state) =>  MaterialPage(child: AddStudents()),
-        ),
-        GoRoute(
-          path: 'add_faculty',
-          pageBuilder: (context, state) => MaterialPage(child: AddFaculty()),
-        ),
-        GoRoute(
-          path: 'add_courses',
-          pageBuilder: (context, state) => MaterialPage(child: AddCourses()),
-        ),
-        GoRoute(
-          path:'view_students',
-          pageBuilder: (context, state) => MaterialPage(child: ViewStudents()),
-        ),
-        GoRoute(
-          path:'view_faculty',
-          pageBuilder: (context, state) => MaterialPage(child: ViewFaculty()),),
-        GoRoute(
-          path:'view_courses',
-          pageBuilder: (context, state) => MaterialPage(child: ViewCourses()),
-        )
-      ]
-    )
+    GoRoute(path: '/', pageBuilder: (context, state) => const MaterialPage(child: AdminHome()), routes: [
+      GoRoute(
+        path: 'add_students',
+        pageBuilder: (context, state) => MaterialPage(child: AddStudents()),
+      ),
+      GoRoute(
+        path: 'add_faculty',
+        pageBuilder: (context, state) => MaterialPage(child: AddFaculty()),
+      ),
+      GoRoute(
+        path: 'add_courses',
+        pageBuilder: (context, state) => MaterialPage(child: AddCourses()),
+      ),
+      GoRoute(
+        path: 'view_students',
+        pageBuilder: (context, state) => MaterialPage(child: ViewStudents()),
+      ),
+      GoRoute(
+        path: 'view_faculty',
+        pageBuilder: (context, state) => MaterialPage(child: ViewFaculty()),
+      ),
+      GoRoute(
+        path: 'view_courses',
+        pageBuilder: (context, state) => MaterialPage(child: ViewCourses()),
+      ),
+      GoRoute(
+        path: 'add_menu',
+        pageBuilder: (context, state) => MaterialPage(child: AddMessMenu()),
+      )
+    ])
   ],
 );
