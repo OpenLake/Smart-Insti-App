5f1b4a9cd74eb2d0352a7d51146c166ba321ffe1
nailsonseat <aadarshv3@gmail.com>
2024-01-21 21:55:08 +0530

    defined providers at root

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_insti_app/constants/constants.dart';
+import 'package:smart_insti_app/provider/admin_provider.dart';
+import 'package:smart_insti_app/provider/courses_provider.dart';
+import 'package:smart_insti_app/provider/menu_provider.dart';
+import 'package:smart_insti_app/provider/student_provider.dart';
 import 'package:smart_insti_app/provider/user_Provider.dart';
 import 'package:smart_insti_app/routes/routes.dart';
 
@@ -15,7 +19,10 @@ class SmartInstiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => StudentProvider()),
+        ChangeNotifierProvider(create: (_) => CoursesProvider()),
+        ChangeNotifierProvider(create: (_) => AdminProvider()),
+        ChangeNotifierProvider(create: (_) => MenuProvider()),
       ],
       child: MaterialApp.router(
         title: AppConstants.appName,
