c2fca2c85f7e49e295a97e22f609ee751347ecf1
nailsonseat <aadarshv3@gmail.com>
2024-01-23 21:23:21 +0530

    Add file picker functionality and search by student name

--- frontend/lib/provider/student_provider.dart
+++ frontend/lib/provider/student_provider.dart
@@ -1,7 +1,10 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:smart_insti_app/constants/constants.dart';
+import 'package:smart_insti_app/constants/dummy_entries.dart';
 import '../models/student.dart';
+import 'dart:io';
 
 class StudentProvider extends ChangeNotifier {
   final TextEditingController _studentNameController = TextEditingController();
@@ -19,16 +22,36 @@ class StudentProvider extends ChangeNotifier {
 
   TextEditingController get searchStudentController => _searchStudentController;
 
-  final List<Student> students = [];
+  final List<Student> students = DummyStudents.students;
   List<Student> filteredStudents = [];
 
   final Logger _logger = Logger();
+
+  void pickSpreadsheet() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      bool isSpreadsheet = result.files.single.path!.endsWith(".xlsx") ||
+          result.files.single.path!.endsWith(".xls") ||
+          result.files.single.path!.endsWith(".csv");
+      if (isSpreadsheet) {
+        File file = File(result.files.single.path!);
+
+        // TODO : Add code to send the spreadsheet to the backend
+
+        _logger.i("Picked file ${file.path}");
+      } else {
+        _logger.e("Picked file is not a spreadsheet");
+      }
+    } else {
+      _logger.e("No file picked");
+    }
+  }
 
   void searchStudents() {
     String query = _searchStudentController.text;
     _logger.i("Searching for student: $query");
     filteredStudents =
-        students.where((student) => student.rollNumber.toLowerCase().contains(query.toLowerCase())).toList();
+        students.where((student) => student.name.toLowerCase().contains(query.toLowerCase())).toList();
     notifyListeners();
   }
 
