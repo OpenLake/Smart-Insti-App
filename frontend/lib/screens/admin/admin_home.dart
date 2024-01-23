54d84a0e47b3c8642498c5ab1be9e8bf24e7805a
nailsonseat <aadarshv3@gmail.com>
2024-01-23 21:47:09 +0530

    Added about and logout dialog pop ups

--- frontend/lib/screens/admin/admin_home.dart
+++ frontend/lib/screens/admin/admin_home.dart
@@ -16,58 +16,91 @@ class AdminHome extends StatelessWidget {
         body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) => [
             CollapsingAppBar(
-                title: 'Welcome\nAdmin',
-                bottom: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Consumer<AdminProvider>(
-                    builder: (_, adminProvider, ___) {
-                      if (adminProvider.toggleSearch) {
-                        return SearchBar(
-                          controller: adminProvider.searchController,
-                          onChanged: (value) => adminProvider.refreshTiles(),
-                          leading: IconButton(
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () {
-                              adminProvider.toggleSearchBar();
-                              adminProvider.searchController.clear();
-                              adminProvider.refreshTiles();
+              body: const Center(
+                child: Text(
+                  'Welcome\nAdmin',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 36,
+                    fontFamily: "RobotoFlex",
+                  ),
+                ),
+              ),
+              bottom: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Consumer<AdminProvider>(
+                  builder: (_, adminProvider, ___) {
+                    if (adminProvider.toggleSearch) {
+                      return SearchBar(
+                        controller: adminProvider.searchController,
+                        onChanged: (value) => adminProvider.refreshTiles(),
+                        leading: IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            adminProvider.toggleSearchBar();
+                            adminProvider.searchController.clear();
+                            adminProvider.refreshTiles();
+                          },
+                        ),
+                        shadowColor: MaterialStateProperty.all(Colors.transparent),
+                      );
+                    } else {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                              iconSize: 30,
+                              onPressed: () => adminProvider.toggleSearchBar(),
+                              icon: const Icon(Icons.search)),
+                          PopupMenuButton(
+                            itemBuilder: (context) {
+                              return [
+                                PopupMenuItem(
+                                  value: "about",
+                                  child: const Text("About"),
+                                  onTap: () => showAboutDialog(
+                                    context: context,
+                                    children: [
+                                      const Text("Smart Insti App"),
+                                      const Text(
+                                          "This app aims to solve the day-to-day problems that students and faculty face in IIT Bhilai and aims to consolidate a lot of useful applications into single app. This could include features like Time Table, Classroom Vacancy, Lost and Found, Chatrooms on various topics like Internet Issues. It could also have a broadcast feature which would be very useful in emergency situations."),
+                                    ],
+                                  ),
+                                ),
+                                PopupMenuItem(
+                                  value: "logout",
+                                  child: const Text("Log Out"),
+                                  onTap: () => showDialog(
+                                    context: context,
+                                    builder: (_) => const AlertDialog(
+                                      title: Text('Log Out'),
+                                      content: Text('Auth will be inplemented in future'),
+                                    ),
+                                  ),
+                                ),
+                              ];
                             },
                           ),
-                          shadowColor: MaterialStateProperty.all(Colors.transparent),
-                        );
-                      } else {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                                iconSize: 30,
-                                onPressed: () => adminProvider.toggleSearchBar(),
-                                icon: const Icon(Icons.search)),
-                            PopupMenuButton(itemBuilder: (context){
-                              return [
-                                const PopupMenuItem(value: "about", child: Text("About Us")),
-                                const PopupMenuItem(value: "logout", child: Text("Log Out")),
-                              ];
-                            }),
-                          ],
-                        );
-                      }
-                    },
-                  ),
-                ))
+                        ],
+                      );
+                    }
+                  },
+                ),
+              ),
+            )
           ],
           body: Container(
-            color: Colors.white,
-            child: Consumer<AdminProvider>(
-              builder: (_,__,___){
-                return GridView.count(
-                  padding: const EdgeInsets.all(10),
-                  crossAxisCount: 2,
-                  children: adminProvider.buildMenuTiles(context),
-                );
-              },
-            )
-          ),
+              color: Colors.white,
+              child: Consumer<AdminProvider>(
+                builder: (_, __, ___) {
+                  return GridView.count(
+                    padding: const EdgeInsets.all(10),
+                    crossAxisCount: 2,
+                    children: adminProvider.buildMenuTiles(context),
+                  );
+                },
+              )),
         ),
       ),
     );
