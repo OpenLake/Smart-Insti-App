1136a89864b0feb1cff0b944b8bd887ffaa85ad6
nailsonseat <aadarshv3@gmail.com>
2024-02-01 20:25:41 +0530

    Added a body param to menu tile

--- frontend/lib/components/menu_tile.dart
+++ frontend/lib/components/menu_tile.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 class MenuTile extends StatelessWidget {
   const MenuTile(
-      {super.key, required this.title, this.icon, required this.onTap, this.primaryColor ,this.secondaryColor });
+      {super.key,
+      required this.title,
+      this.icon,
+      required this.onTap,
+      required this.primaryColor,
+      required this.secondaryColor,
+      this.body,
+      this.contentPadding});
 
   final String title;
+  final List<Widget>? body;
   final IconData? icon;
   final Function onTap;
-  final Color? primaryColor;
-  final Color? secondaryColor;
+  final EdgeInsets? contentPadding;
+  final Color primaryColor;
+  final Color secondaryColor;
 
   @override
   Widget build(BuildContext context) {
@@ -17,23 +26,29 @@ class MenuTile extends StatelessWidget {
       child: Material(
         borderRadius: BorderRadius.circular(15),
         child: Ink(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color:primaryColor ?? Colors.grey[100],
-          ),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: primaryColor),
           child: InkWell(
-            overlayColor: MaterialStateProperty.all<Color?>(secondaryColor ?? Colors.grey[300]),
+            overlayColor: MaterialStateProperty.all<Color?>(secondaryColor),
             borderRadius: BorderRadius.circular(15),
-            splashColor: secondaryColor ?? Colors.grey[200],
+            splashColor: secondaryColor,
             onTap: () => onTap(),
             child: Center(
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 21,
-                  fontFamily: "GoogleSansFlex",
+              child: Padding(
+                padding: contentPadding ?? const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                        Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 21,
+                            fontFamily: "GoogleSansFlex",
+                          ),
+                        ),
+                      ] +
+                      (body ?? []),
                 ),
               ),
             ),
