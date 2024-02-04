54b24e6cf2cf1eb570471b54cfa592984714c1fd
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2024-02-05 00:46:26 +0530

    removed unused import

--- frontend/lib/provider/about_Edit_widget.dart
+++ frontend/lib/provider/about_Edit_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../provider/student_provider.dart';
+// import '../provider/student_provider.dart';
 
 final aboutProvider = Provider<String?>((ref) {
   return ref.watch(aboutControllerProvider);
