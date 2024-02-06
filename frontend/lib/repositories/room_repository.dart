da034bbddfdb7ee129ca04af198c99558b9bcbff
nailsonseat <aadarshv3@gmail.com>
2024-02-06 16:06:48 +0530

    Updated get rooms

--- frontend/lib/repositories/room_repository.dart
+++ frontend/lib/repositories/room_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:smart_insti_app/constants/dummy_entries.dart';
 import '../models/room.dart';
 
@@ -8,14 +9,18 @@ final roomRepositoryProvider = Provider<RoomRepository>((_) => RoomRepository())
 class RoomRepository {
   final _client = Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:3000',
+      baseUrl: 'http://10.0.2.2:3000',
     ),
   );
 
   Future<List<Room>> getRooms() async {
     try {
       final response = await _client.get('/rooms');
-      return response.data.map((e) => Room.fromJson(e)).toList();
+      List<Room> rooms = [];
+      for (var room in response.data) {
+        rooms.add(Room.fromJson(room));
+      }
+      return rooms;
     } catch (e) {
       return DummyRooms.rooms;
     }
