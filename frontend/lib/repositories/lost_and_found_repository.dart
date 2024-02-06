29c13099042ddcd1e0e909c39019b6c077b17653
nailsonseat <aadarshv3@gmail.com>
2024-02-06 16:24:56 +0530

    Updated to add and fetch lost and found item logic

--- frontend/lib/repositories/lost_and_found_repository.dart
+++ frontend/lib/repositories/lost_and_found_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:smart_insti_app/constants/dummy_entries.dart';
 import 'package:smart_insti_app/models/lost_and_found_item.dart';
 
@@ -8,16 +9,47 @@ final lostAndFoundRepositoryProvider = Provider<LostAndFoundRepository>((_) => L
 class LostAndFoundRepository {
   final _client = Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:3000',
+      baseUrl: 'http://10.0.2.2:3000',
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     ),
   );
 
   Future<List<LostAndFoundItem>> lostAndFoundItems() async {
     try {
       final response = await _client.get('/lost-and-found');
-      return response.data.map((e) => LostAndFoundItem.fromJson(e)).toList();
+      List<LostAndFoundItem> items = [];
+      for (var item in response.data) {
+        items.add(LostAndFoundItem.fromJson(item));
+      }
+      return items;
     } catch (e) {
+      Logger().e(e);
       return DummyLostAndFound.lostAndFoundItems;
+    }
+  }
+
+  Future<void> addLostAndFoundItem(LostAndFoundItem item) async {
+    try {
+      String? fileName = item.imagePath?.split('/').last;
+      FormData formData = FormData.fromMap({
+        "name": item.name,
+        "lastSeenLocation": item.lastSeenLocation,
+        "description": item.description,
+        "contactNumber": item.contactNumber,
+        "isLost": item.isLost,
+        "image": item.imagePath != null
+            ? await MultipartFile.fromFile(
+                item.imagePath!,
+                filename: fileName,
+              )
+            : null,
+      });
+      final response = await _client.post('/lost-and-found', data: formData);
+      Logger().w(response.data);
+    } catch (e) {
+      Logger().e(e);
     }
   }
 }
