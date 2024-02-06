869ba643ec3642006fc4b07c8b2b5b94955a0cb6
nailsonseat <aadarshv3@gmail.com>
2024-02-06 15:18:43 +0530

    Formatted auth resource

--- backend/resources/authResource.js
+++ backend/resources/authResource.js
@@ -1,33 +1,33 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import * as errorMessages from '../constants/errorMessages.js';
-import Student from '../models/Student.js';
-import Faculty from '../models/faculty.js';
-import Admin from '../models/admin.js';
+import mongoose from "mongoose";
+import express from "express";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import * as errorMessages from "../constants/errorMessages.js";
+import Student from "../models/student.js";
+import Faculty from "../models/faculty.js";
+import Admin from "../models/admin.js";
 
 const authRouter = express.Router();
 
 // Sign-Up Route
-authRouter.post('/signup',async (req,res)=>{
-   try {
-        const {email, password } = req.body;
-        const existingUser = await Admin.findOne({ email });
-        if (existingUser){
-            return res.status(400).json({ msg: errorMessages.userAlreadyExists });
-        }
-        const hashedPassword = await bcryptjs.hash(password,8);
-        let admin = new Admin({
-            email,
-            password: hashedPassword,
-        });
-        admin = await admin.save();
-        res.json(admin);
-    } catch (e) {
-        res.status(500).json({error:e.message});
+authRouter.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await Admin.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: errorMessages.userAlreadyExists });
     }
-})
+    const hashedPassword = await bcryptjs.hash(password, 8);
+    let admin = new Admin({
+      email,
+      password: hashedPassword,
+    });
+    admin = await admin.save();
+    res.json(admin);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 // Sign-In Route
 authRouter.post("/signin", async (req, res) => {
@@ -36,13 +36,11 @@ authRouter.post("/signin", async (req, res) => {
 
     const user = await Admin.findOne({ email });
     if (!user) {
-      return res
-        .status(400)
-        .json({ msg: errorMessages.userNotFound });
+      return res.status(400).json({ msg: errorMessages.userNotFound });
     }
 
     const isMatch = await bcryptjs.compare(password, user.password);
-    
+
     if (!isMatch) {
       return res.status(400).json({ msg: errorMessages.incorrectPassword });
     }
@@ -54,26 +52,28 @@ authRouter.post("/signin", async (req, res) => {
   }
 });
 
-authRouter.post('/login', async (req, res) => {
+authRouter.post("/login", async (req, res) => {
   const { email, userType } = req.body;
-  const token = jwt.sign({ email,userType }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign({ email, userType }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
   let userCollection;
   let existingUser;
-  
-  switch(userType) {
-    case 'student':
+
+  switch (userType) {
+    case "student":
       userCollection = Student;
       break;
-    case 'faculty':
+    case "faculty":
       userCollection = Faculty;
       break;
-    case 'admin':
+    case "admin":
       userCollection = Admin;
       break;
     default:
       return res.status(400).send({ error: errorMessages.invalidUserType });
   }
-  
+
   existingUser = await userCollection.findOne({ email });
 
   if (!existingUser) {
@@ -81,8 +81,8 @@ authRouter.post('/login', async (req, res) => {
     await newUser.save();
     res.send({ message: errorMessages.userCreated, user: newUser });
   } else {
-    res.send({ message: errorMessages.userAlreadyExists, user: existingUser});
+    res.send({ message: errorMessages.userAlreadyExists, user: existingUser });
   }
 });
 
-export default authRouter
+export default authRouter;
