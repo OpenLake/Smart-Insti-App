285d538990d637e06fa440d844798f27ad6fc998
nailsonseat <aadarshv3@gmail.com>
2024-02-06 17:09:22 +0530

    Fixed to json method in LNF repository

--- frontend/lib/models/lost_and_found_item.dart
+++ frontend/lib/models/lost_and_found_item.dart
@@ -1,17 +1,19 @@
 // This class is used to create an object of Lost and Found item
 
 class LostAndFoundItem {
+  String? id;
   String name;
   String lastSeenLocation;
-  String imagePath;
+  String? imagePath;
   String description;
   String contactNumber;
   bool isLost;
 
   LostAndFoundItem({
+    this.id,
     required this.name,
     required this.lastSeenLocation,
-    required this.imagePath,
+    this.imagePath,
     required this.description,
     required this.isLost,
     required this.contactNumber,
@@ -19,12 +21,13 @@ class LostAndFoundItem {
 
   factory LostAndFoundItem.fromJson(Map<String, dynamic> json) {
     return LostAndFoundItem(
+      id: json['_id'],
       name: json['name'],
       lastSeenLocation: json['lastSeenLocation'],
       imagePath: json['imagePath'],
       description: json['description'],
       isLost: json['isLost'],
-      contactNumber: json['contact'],
+      contactNumber: json['contactNumber'],
     );
   }
 
