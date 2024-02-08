4401c0ed2002c2ca4abf79095508be23d04c96c9
nailsonseat <aadarshv3@gmail.com>
2024-02-09 01:57:37 +0530

    Update authResource import and route handler in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import testResource from "./resources/testResource.js";
-
-import authResource from "./resources/authResource.js";
+import authResource from "./resources/generalAuthResource.js";
 import otpResource from "./resources/otpResource.js";
 import Connection from "./database/db.js";
-import bodyParser from "body-parser";
 import cors from "cors";
-import auth from "./middlewares/auth.js";
-const PORT =`${process.env.PORT || 3000}`;
+import tokenRequired from "./middlewares/tokenRequired.js";
+const PORT = `${process.env.PORT || 3000}`;
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
@@ -24,7 +22,7 @@ app.use(authResource);
 app.use(otpResource);
 app.use("/", testResource);
 
-app.get('/protected', auth, (req, res) => {
-  res.json({ message: 'Access granted' });
+app.get("/protected", tokenRequired, (req, res) => {
+  res.json({ message: "Access granted" });
 });
 export default app;
