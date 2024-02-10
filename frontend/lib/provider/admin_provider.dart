2615af58e68172bdb00b8ee35ba4fa70ddd8165a
nailsonseat <aadarshv3@gmail.com>
2024-02-10 18:25:23 +0530

    Updated page paths

--- frontend/lib/provider/admin_provider.dart
+++ frontend/lib/provider/admin_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:logger/logger.dart';
 import 'package:smart_insti_app/components/menu_tile.dart';
+import 'package:smart_insti_app/models/admin.dart';
 
-final adminProvider =
-    StateNotifierProvider<AdminNotifier, AdminState>((ref) => AdminNotifier());
+final adminProvider = StateNotifierProvider<AdminStateNotifier, AdminState>((ref) => AdminStateNotifier());
 
 class AdminState {
   final bool toggleSearch;
@@ -18,6 +19,7 @@ class AdminState {
   });
 
   AdminState copyWith({
+    Admin? currentAdmin,
     bool? toggleSearch,
     TextEditingController? searchController,
     List<MenuTile>? menuTiles,
@@ -29,81 +31,77 @@ class AdminState {
   }
 }
 
-class AdminNotifier extends StateNotifier<AdminState> {
-  AdminNotifier()
+class AdminStateNotifier extends StateNotifier<AdminState> {
+  AdminStateNotifier()
       : super(AdminState(
           searchController: TextEditingController(),
           toggleSearch: false,
           menuTiles: [],
         ));
 
-  get searchController => state.searchController;
-
-  get toggleSearch => state.toggleSearch;
-
-  get menuTiles => state.menuTiles;
+  final Logger _logger = Logger();
 
   void buildMenuTiles(BuildContext context) {
     List<MenuTile> menuTiles = [
       MenuTile(
         title: "Add\nStudents",
-        onTap: () => context.push('/admin_home/add_students'),
+        onTap: () => context.push('/admin/add_students'),
         icon: Icons.add,
         primaryColor: Colors.redAccent.shade100,
         secondaryColor: Colors.redAccent.shade200,
       ),
       MenuTile(
         title: "View\nStudents",
-        onTap: () => context.push('/admin_home/view_students'),
+        onTap: () => context.push('/admin/view_students'),
         icon: Icons.add,
         primaryColor: Colors.greenAccent.shade100,
         secondaryColor: Colors.greenAccent.shade200,
       ),
       MenuTile(
         title: "Add\nCourses",
-        onTap: () => context.push('/admin_home/add_courses'),
+        onTap: () => context.push('/admin/add_courses'),
         icon: Icons.add,
         primaryColor: Colors.yellowAccent.shade100,
         secondaryColor: Colors.yellowAccent.shade200,
       ),
       MenuTile(
         title: "View\nCourses",
-        onTap: () => context.push('/admin_home/view_courses'),
+        onTap: () => context.push('/admin/view_courses'),
         icon: Icons.add,
         primaryColor: Colors.lightBlueAccent.shade100,
         secondaryColor: Colors.lightBlueAccent.shade200,
       ),
       MenuTile(
         title: "Add\nFaculty",
-        onTap: () => context.push('/admin_home/add_faculty'),
+        onTap: () => context.push('/admin/add_faculty'),
         icon: Icons.add,
         primaryColor: Colors.purpleAccent.shade100,
         secondaryColor: Colors.purpleAccent.shade200,
       ),
       MenuTile(
         title: "View\nFaculty",
-        onTap: () => context.push('/admin_home/view_faculty'),
+        onTap: () => context.push('/admin/view_faculty'),
         icon: Icons.add,
         primaryColor: Colors.orangeAccent.shade100,
         secondaryColor: Colors.orangeAccent.shade200,
       ),
       MenuTile(
         title: "Add\nMess\nMenu",
-        onTap: () => context.push('/admin_home/add_menu'),
+        onTap: () => context.push('/admin/add_menu'),
         icon: Icons.add,
         primaryColor: Colors.pinkAccent.shade100,
         secondaryColor: Colors.pinkAccent.shade200,
       ),
       MenuTile(
         title: "View\nMess\nMenu",
-        onTap: () => context.push('/admin_home/view_menu'),
+        onTap: () => context.push('/admin/view_menu'),
         icon: Icons.add,
         primaryColor: Colors.blueAccent.shade100,
         secondaryColor: Colors.blueAccent.shade200,
       ),
       MenuTile(
         title: "Manage\nRooms",
-        onTap: () => context.push('/admin_home/manage_rooms'),
+        onTap: () => context.push('/admin/manage_rooms'),
         icon: Icons.add,
         primaryColor: Colors.tealAccent.shade100,
         secondaryColor: Colors.tealAccent.shade200,
@@ -111,10 +109,7 @@ class AdminNotifier extends StateNotifier<AdminState> {
     ];
     String query = state.searchController.text;
     state = state.copyWith(
-        menuTiles: menuTiles
-            .where((element) =>
-                element.title.toLowerCase().contains(query.toLowerCase()))
-            .toList());
+        menuTiles: menuTiles.where((element) => element.title.toLowerCase().contains(query.toLowerCase())).toList());
   }
 
   void toggleSearchBar() {
