292391869b7eeb554b803147e4713b0ebb24aa07
nailsonseat <aadarshv3@gmail.com>
2024-02-10 17:53:47 +0530

    added the new resources

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import testResource from "./resources/testResource.js";
 import authResource from "./resources/generalAuthResource.js";
+import adminAuthResource from "./resources/adminAuthResource.js";
 import otpResource from "./resources/otpResource.js";
 import Connection from "./database/db.js";
 import cors from "cors";
 import tokenRequired from "./middlewares/tokenRequired.js";
+import adminResource from "./resources/adminResource.js";
+
 const PORT = `${process.env.PORT || 3000}`;
 const app = express();
 
@@ -18,8 +21,10 @@ app.use(cors());
 
 // Get Database connection
 Connection();
+app.use("/admin", adminResource);
+app.use("/admin-auth", adminAuthResource);
 app.use(authResource);
-app.use(otpResource);
+app.use("otp", otpResource);
 app.use("/", testResource);
 
 app.get("/protected", tokenRequired, (req, res) => {
