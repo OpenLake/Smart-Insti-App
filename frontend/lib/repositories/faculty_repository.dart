02999d0d87edb397e1fe6a49f270a324c274d8b9
Arin Nigam <arin.nigam@gmail.com>
2024-02-11 17:41:30 +0530

    updated faculty_repository

--- frontend/lib/repositories/faculty_repository.dart
+++ frontend/lib/repositories/faculty_repository.dart
@@ -1,10 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:smart_insti_app/models/Faculty.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../constants/dummy_entries.dart';
+import '../models/faculty.dart';
 
 final facultyRepositoryProvider =
     Provider<FacultyRepository>((_) => FacultyRepository());
+final storage = FlutterSecureStorage();
 
 class FacultyRepository {
   final _client = Dio(
@@ -13,12 +16,58 @@ class FacultyRepository {
     ),
   );
 
-  Future<List<dynamic>> Faculties() async {
+  Future<List<Faculty>> faculties() async {
     try {
       final response = await _client.get('/faculties');
-      return response.data.map((e) => Faculty.fromJson(e)).toList();
+      return (response.data as List).map((e) => Faculty.fromJson(e)).toList();
     } catch (e) {
       return DummyFaculties.faculties;
+    }
+  }
+
+  Future<Faculty> getFaculty(String email) async {
+    try {
+      final id = await storage.read(key: email);
+      final response = await _client.get('/faculties/$id');
+      return Faculty.fromJson(response.data);
+    } catch (e) {
+      return DummyFaculties.faculties[0];
+    }
+  }
+
+  Future<Faculty> addFaculty(String email) async {
+    try {
+      final response = await _client.post(
+        '/faculties',
+        data: {
+          'email': email,
+        },
+      );
+
+      storage.write(
+          key: response.data['user']['email'], value: response.data['_id']);
+
+      return Faculty.fromJson(response.data['user']);
+    } catch (e) {
+      return DummyFaculties.faculties[0];
+    }
+  }
+
+  Future<Faculty> updateFaculty(Faculty faculty) async {
+    try {
+      final response =
+          await _client.put('/faculties/${faculty.id}', data: faculty.toJson());
+      return Faculty.fromJson(response.data['user']);
+    } catch (e) {
+      return DummyFaculties.faculties[0];
+    }
+  }
+
+  Future<void> deleteFaculty(String id) async {
+    try {
+      await _client.delete('/faculties/$id');
+    } catch (e) {
+      return;
     }
   }
 }
