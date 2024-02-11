bb7a59f6d59e8bbf705ef3c40734649eb900e912
Arin Nigam <arin.nigam@gmail.com>
2024-02-11 17:23:37 +0530

    updated student_repository

--- frontend/lib/repositories/student_repository.dart
+++ frontend/lib/repositories/student_repository.dart
@@ -1,10 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:smart_insti_app/models/student.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../constants/dummy_entries.dart';
+import '../models/student.dart';
+import '../provider/user_provider.dart';
 
 final studentRepositoryProvider =
     Provider<StudentRepository>((_) => StudentRepository());
+final storage = FlutterSecureStorage();
 
 class StudentRepository {
   final _client = Dio(
@@ -13,13 +17,57 @@ class StudentRepository {
     ),
   );
 
-  Future<List<dynamic>> Students() async {
+  Future<List<Student>> students() async {
     try {
       final response = await _client.get('/students');
-      return response.data.map((e) => Student.fromJson(e)).toList();
+      return (response.data as List).map((e) => Student.fromJson(e)).toList();
     } catch (e) {
-      print(e);
       return DummyStudents.students;
+    }
+  }
+
+  Future<Student> getStudent(String? id) async {
+    try {
+      final response = await _client.get('/students/$id');
+      return Student.fromJson(response.data);
+    } catch (e) {
+      return DummyStudents.students[0];
+    }
+  }
+
+  Future<Student> addStudent(String email) async {
+    try {
+      final response = await _client.post(
+        '/students',
+        data: {
+          'email': email,
+        },
+      );
+
+      storage.write(
+          key: response.data['user']['email'], value: response.data['_id']);
+
+      return Student.fromJson(response.data['user']);
+    } catch (e) {
+      return DummyStudents.students[0];
+    }
+  }
+
+  Future<Student> updateStudent(state) async {
+    try {
+      final response = await _client.put('/students/${state.student.id}',
+          data: state.student.toJson());
+      return Student.fromJson(response.data['user']);
+    } catch (e) {
+      return DummyStudents.students[0];
+    }
+  }
+
+  Future<void> deleteStudent(String id) async {
+    try {
+      await _client.delete('/students/$id');
+    } catch (e) {
+      return;
     }
   }
 }
