6e46de35bdd4a84a43f52df8f35cc2e1909e6360
Arin Nigam <arin.nigam@gmail.com>
2024-02-12 11:40:05 +0530

    updated routes

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -5,43 +5,30 @@ import 'package:smart_insti_app/screens/admin/add_students.dart';
 import 'package:smart_insti_app/screens/admin/admin_home.dart';
 import 'package:smart_insti_app/screens/admin/manage_rooms.dart';
 import 'package:smart_insti_app/screens/admin/view_students.dart';
-import 'package:smart_insti_app/screens/home.dart';
+import 'package:smart_insti_app/screens/auth/user_login.dart';
 import 'package:smart_insti_app/screens/lost_and_found.dart';
 import '../screens/admin/add_faculty.dart';
 import '../screens/admin/add_menu.dart';
 import '../screens/admin/view_courses.dart';
 import '../screens/admin/view_faculty.dart';
 import '../screens/admin/view_menu.dart';
-import '../screens/auth/signin_page.dart';
+import '../screens/auth/admin_login.dart';
+
 import '../screens/room_vacancy.dart';
+import '../screens/user/user_home.dart';
+import '../screens/user_profile.dart';
 
 final GoRouter routes = GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      pageBuilder: (context, state) => MaterialPage(child: SignIn()),
+      pageBuilder: (context, state) => MaterialPage(child: UserLogin()),
       routes: [
         GoRoute(
-          path: 'admin_home',
-          pageBuilder: (context, state) =>
-              const MaterialPage(child: AdminHome()),
+          path: 'user_home',
+          pageBuilder: (context, state) => MaterialPage(child: UserHome()),
           routes: [
-            GoRoute(
-              path: 'add_students',
-              pageBuilder: (context, state) =>
-                  MaterialPage(child: AddStudents()),
-            ),
-            GoRoute(
-              path: 'add_faculty',
-              pageBuilder: (context, state) =>
-                  MaterialPage(child: AddFaculty()),
-            ),
-            GoRoute(
-              path: 'add_courses',
-              pageBuilder: (context, state) =>
-                  MaterialPage(child: AddCourses()),
-            ),
             GoRoute(
               path: 'view_students',
               pageBuilder: (context, state) =>
@@ -58,35 +45,89 @@ final GoRouter routes = GoRouter(
                   MaterialPage(child: ViewCourses()),
             ),
             GoRoute(
-              path: 'add_menu',
-              pageBuilder: (context, state) =>
-                  MaterialPage(child: AddMessMenu()),
-            ),
-            GoRoute(
               path: 'view_menu',
               pageBuilder: (context, state) =>
                   MaterialPage(child: ViewMessMenu()),
             ),
             GoRoute(
+              path: 'classroom_vacancy',
+              pageBuilder: (context, state) =>
+                  const MaterialPage(child: RoomVacancy()),
+            ),
+            GoRoute(
+              path: 'lost_and_found',
+              pageBuilder: (context, state) =>
+                  MaterialPage(child: LostAndFound()),
+            ),
+            GoRoute(
               path: 'manage_rooms',
               pageBuilder: (context, state) =>
                   MaterialPage(child: ManageRooms()),
-            )
+            ),
+            GoRoute(
+              path: 'user_profile',
+              pageBuilder: (context, state) =>
+                  MaterialPage(child: UserProfile()),
+            ),
           ],
         ),
-      ],
-    ),
-    GoRoute(
-      path: '/home',
-      pageBuilder: (context, state) => const MaterialPage(child: Home()),
-      routes: [
         GoRoute(
-          path: 'classroom_vacancy',
-          pageBuilder: (context, state) => const MaterialPage(child: RoomVacancy()),
-        ),
-        GoRoute(
-          path: 'lost_and_found',
-          pageBuilder: (context, state) => MaterialPage(child: LostAndFound()),
+          path: 'admin_login',
+          pageBuilder: (context, state) => MaterialPage(child: AdminLogin()),
+          routes: [
+            GoRoute(
+              path: 'admin_home',
+              pageBuilder: (context, state) =>
+                  const MaterialPage(child: AdminHome()),
+              routes: [
+                GoRoute(
+                  path: 'add_students',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: AddStudents()),
+                ),
+                GoRoute(
+                  path: 'add_faculty',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: AddFaculty()),
+                ),
+                GoRoute(
+                  path: 'add_courses',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: AddCourses()),
+                ),
+                GoRoute(
+                  path: 'view_students',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: ViewStudents()),
+                ),
+                GoRoute(
+                  path: 'view_faculty',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: ViewFaculty()),
+                ),
+                GoRoute(
+                  path: 'view_courses',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: ViewCourses()),
+                ),
+                GoRoute(
+                  path: 'add_menu',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: AddMessMenu()),
+                ),
+                GoRoute(
+                  path: 'view_menu',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: ViewMessMenu()),
+                ),
+                GoRoute(
+                  path: 'manage_rooms',
+                  pageBuilder: (context, state) =>
+                      MaterialPage(child: ManageRooms()),
+                ),
+              ],
+            ),
+          ],
         ),
       ],
     ),
