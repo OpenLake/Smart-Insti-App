d94f9f0103acf2b5481fe0bf11e6bd5ae1cf4700
nailsonseat <aadarshv3@gmail.com>
2024-02-13 01:30:59 +0530

    faculty repository initial commit

---
+++ frontend/lib/repositories/faculty_repository.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+import '../models/faculty.dart';
+
+final facultyRepositoryProvider = Provider<FacultyRepository>((ref) => FacultyRepository());
+
+class FacultyRepository {
+  final _client = Dio(
+    BaseOptions(
+      baseUrl: dotenv.env['BACKEND_DOMAIN']!,
+    ),
+  );
+
+  final Logger _logger = Logger();
+
+  Future<Faculty?> getFacultyById(String id) async {
+    try {
+      final response = await _client.get('/faculty/$id');
+      return Faculty.fromJson(response.data);
+    } catch (e) {
+      _logger.e(e);
+      return null;
+    }
+  }
+}
