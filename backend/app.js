23517b910c0c8685109d3532b363cbeb78bb5433
nailsonseat <aadarshv3@gmail.com>
2024-02-13 01:33:00 +0530

    Update app.js with new resource imports

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import testResource from "./resources/testResource.js";
-import authResource from "./resources/generalAuthResource.js";
+import generalAuthResource from "./resources/generalAuthResource.js";
 import adminAuthResource from "./resources/adminAuthResource.js";
 import otpResource from "./resources/otpResource.js";
 import Connection from "./database/db.js";
 import cors from "cors";
 import tokenRequired from "./middlewares/tokenRequired.js";
 import adminResource from "./resources/adminResource.js";
-
 import roomListResource from "./resources/rooms/roomListResource.js";
 import roomResource from "./resources/rooms/roomResource.js";
 import lostAndFoundListResource from "./resources/lostAndFound/lostAndFoundListResource.js";
+import studentResource from "./resources/studentResource.js";
+import facultyResource from "./resources/facultyResource.js";
 
 const PORT = `${process.env.PORT || 3000}`;
 const app = express();
@@ -26,9 +27,11 @@ app.use(cors());
 // Get Database connection
 Connection();
 app.use("/admin", adminResource);
+app.use("/student", studentResource);
+app.use("/faculty", facultyResource);
 app.use("/admin-auth", adminAuthResource);
-app.use(authResource);
-app.use("otp", otpResource);
+app.use("/general-auth", generalAuthResource);
+app.use("/otp", otpResource);
 app.use("/", testResource);
 app.use("/rooms", roomListResource);
 app.use("/room", roomResource);
