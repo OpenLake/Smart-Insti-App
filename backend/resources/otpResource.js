c1134f21b0d7dede2b077b1fabe9475653ffc772
nailsonseat <aadarshv3@gmail.com>
2024-02-13 01:32:45 +0530

    Check user existence before sending otp logic added

--- backend/resources/otpResource.js
+++ backend/resources/otpResource.js
@@ -24,9 +24,29 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 otpRouter.post("/send-otp", async (req, res) => {
-  const { email } = req.body;
-  if (!email) {
-    return res.status(400).json({ error: messages.emailIsRequired });
+  const { email, loginForRole } = req.body;
+
+  if (!email || !loginForRole) {
+    return res.status(412).json({ error: messages.emailIsRequired });
+  }
+
+  const studentCollectionName = "students";
+  const facultyCollectionName = "faculties";
+
+  // Check if the email exists in the database by loginForRole which can be wither student or faculty
+  try {
+    await client.connect();
+    const database = client.db(dbName);
+    const collection = database.collection(
+      loginForRole === "student" ? studentCollectionName : facultyCollectionName
+    );
+    const result = await collection.findOne({ email });
+
+    if (!result) {
+      return res.status(400).json({ message: messages.userNotFound });
+    }
+  } finally {
+    await client.close();
   }
   // Generate OTP
   const otp = generateOTP();
@@ -37,10 +57,10 @@ otpRouter.post("/send-otp", async (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      return res.status(500).json({ error: messages.failedToSendOTPEmail });
+      return res.status(400).json({ message: messages.failedToSendOTPEmail });
     } else {
       console.log("Message sent: %s", info.response);
-      res.json({ message: messages.otpSent, email });
+      res.json({ message: messages.otpSent + " to " + email });
     }
   });
 
@@ -52,9 +72,8 @@ otpRouter.post("/send-otp", async (req, res) => {
     await collection.deleteMany({
       email: email,
     });
-
-    await collection.insertOne({ email, otp });
-    console.log("OTP data stored in MongoDB");
+    const createdAt = new Date();
+    await collection.insertOne({ email, otp, createdAt });
   } finally {
     await client.close();
   }
@@ -64,7 +83,7 @@ otpRouter.post("/verify-otp", async (req, res) => {
   const { email, otp } = req.body;
 
   if (!email || !otp) {
-    return res.status(400).json({ error: messages.emailAndOTPRequired });
+    return res.status(412).json({ message: messages.emailAndOTPRequired });
   }
 
   try {
@@ -75,7 +94,7 @@ otpRouter.post("/verify-otp", async (req, res) => {
     const result = await otpCollection.findOne({ email });
 
     if (!result || !result.otp) {
-      return res.status(400).json({ error: messages.noOTPFoundForEmail });
+      return res.status(400).json({ message: messages.otpExpired });
     }
 
     const storedOTP = result.otp;
@@ -83,11 +102,11 @@ otpRouter.post("/verify-otp", async (req, res) => {
     // Compare the provided OTP with the stored OTP
     if (otp === storedOTP) {
       await otpCollection.deleteOne({ email });
-      return res.json({ message: messages.otpVerfied });
+      return res.status(200).json({ message: messages.otpVerfied });
     }
-    res.status(401).json({ error: messages.incorrectOTP });
+    res.status(401).json({ message: messages.incorrectOTP });
   } catch (err) {
-    res.status(500).json({ error: messages.internalServerError });
+    res.status(500).json({ message: messages.internalServerError });
   } finally {
     await client.close();
   }
