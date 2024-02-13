2860dab3af7e18310b53705e0edecf74e7db1672
nailsonseat <aadarshv3@gmail.com>
2024-02-13 17:51:50 +0530

    Removed redundant features of home rovider already present in auth provider

--- frontend/lib/provider/home_provider.dart
+++ frontend/lib/provider/home_provider.dart
@@ -2,73 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:logger/logger.dart';
-
+import 'package:smart_insti_app/provider/room_provider.dart';
 import '../components/menu_tile.dart';
 import '../models/student.dart';
 import '../models/faculty.dart';
 
-import '../repositories/student_repository.dart';
-import '../repositories/faculty_repository.dart';
+final homeProvider =
+    StateNotifierProvider<UserProvider, HomeState>((ref) => UserProvider(ref));
 
-final userProvider =
-    StateNotifierProvider<UserProvider, UserState>((ref) => UserProvider(ref));
-
-class UserState {
-  final Student student;
-  final Faculty faculty;
+class HomeState {
   final bool toggleSearch;
   final TextEditingController searchController;
   final List<MenuTile> menuTiles;
 
-  UserState({
-    required this.student,
-    required this.faculty,
+  HomeState({
     required this.toggleSearch,
     required this.searchController,
     required this.menuTiles,
   });
 
-  UserState copyWith({
+  HomeState copyWith({
     Student? student,
     Faculty? faculty,
     bool? toggleSearch,
     TextEditingController? searchController,
     List<MenuTile>? menuTiles,
   }) {
-    return UserState(
-        student: student ?? this.student,
-        faculty: faculty ?? this.faculty,
+    return HomeState(
         toggleSearch: toggleSearch ?? this.toggleSearch,
         searchController: searchController ?? this.searchController,
         menuTiles: menuTiles ?? this.menuTiles);
   }
 }
 
-class UserProvider extends StateNotifier<UserState> {
+class UserProvider extends StateNotifier<HomeState> {
   UserProvider(Ref ref)
       : super(
-          UserState(
-            student: Student(
-              id: '',
-              name: '',
-              email: '',
-              rollNumber: '',
-              about: '',
-              profilePicURI: '',
-              branch: '',
-              graduationYear: null,
-              skills: [],
-              achievements: [],
-              roles: [],
-            ),
-            faculty: Faculty(
-              id: '',
-              name: '',
-              email: '',
-              department: '',
-              cabinNumber: '',
-              courses: [],
-            ),
+          HomeState(
             searchController: TextEditingController(),
             toggleSearch: false,
             menuTiles: [],
@@ -85,43 +55,18 @@ class UserProvider extends StateNotifier<UserState> {
   void buildMenuTiles(BuildContext context) {
     List<MenuTile> menuTiles = [
       MenuTile(
-        title: "View\nStudents",
-        onTap: () => context.push('/user_home/view_students'),
-        icon: Icons.add,
-        primaryColor: Colors.greenAccent.shade100,
-        secondaryColor: Colors.greenAccent.shade200,
-      ),
-      MenuTile(
-        title: "View\nCourses",
-        onTap: () => context.push('/user_home/view_courses'),
-        icon: Icons.add,
-        primaryColor: Colors.lightBlueAccent.shade100,
-        secondaryColor: Colors.lightBlueAccent.shade200,
-      ),
-      MenuTile(
-        title: "View\nFaculty",
-        onTap: () => context.push('/user_home/view_faculty'),
-        icon: Icons.add,
-        primaryColor: Colors.orangeAccent.shade100,
-        secondaryColor: Colors.orangeAccent.shade200,
-      ),
-      MenuTile(
-        title: "View\nMess\nMenu",
-        onTap: () => context.push('/user_home/view_menu'),
-        icon: Icons.add,
-        primaryColor: Colors.blueAccent.shade100,
-        secondaryColor: Colors.blueAccent.shade200,
-      ),
-      MenuTile(
-        title: "View\nRooms",
-        onTap: () => context.push('/user_home/manage_rooms'),
-        icon: Icons.add,
-        primaryColor: Colors.tealAccent.shade100,
-        secondaryColor: Colors.tealAccent.shade200,
-      ),
-      MenuTile(
         title: 'Room\nVacancy',
-        onTap: () => context.push('/user_home/classroom_vacancy'),
+        onTap: () => context.push('/user_home/room_vacancy'),
+        body: [
+          const SizedBox(height: 5),
+          Consumer(
+            builder: (_, ref, __) => Text(
+              '${ref.watch(roomProvider).roomList.where((element) => element.vacant).length} Vacant',
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
         icon: Icons.add,
         primaryColor: Colors.pinkAccent.shade100,
         secondaryColor: Colors.pinkAccent.shade200,
