289f49024a1c48e3e22c3a55834ef7872b97606c
nailsonseat <aadarshv3@gmail.com>
2024-02-13 15:11:31 +0530

    Refactor token verification in tokenRequired middleware

--- backend/middlewares/tokenRequired.js
+++ backend/middlewares/tokenRequired.js
@@ -1,24 +1,27 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
 const secretKey = process.env.ACCESS_TOKEN_SECRET;
+dotenv.config();
 
 const tokenRequired = async (req, res, next) => {
   try {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      const bearerToken = bearer[1];
-      // Verify the token
-      jwt.verify(bearerToken, secretKey, (err, user) => {
-        if (err) {
-          res.sendStatus(403);
-        } else {
-          req.user = user;
-          next();
-        }
-      });
+    const token = req.headers["authorization"];
+    if (!token || token == "null" || token == "undefined") {
+      return res.status(401).json({ message: "Token is required" });
     }
+
+    // Verify the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+      // If token is valid, attach the decoded payload to the request object
+      req.user = decoded;
+      next();
+    });
   } catch (error) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
