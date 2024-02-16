8e6658ca7e5eee0332e845941b519b7c326cd2bb
nailsonseat <aadarshv3@gmail.com>
2024-02-16 14:34:38 +0530

    Added prompt for adding timetable

--- frontend/lib/screens/user/timetables.dart
+++ frontend/lib/screens/user/timetables.dart
@@ -22,29 +22,97 @@ class Timetables extends StatelessWidget {
             showDialog(
               context: context,
               builder: (_) => AlertDialog(
-                content: const SizedBox(
-                  width: 300,
-                  height: 250,
+                    content: SizedBox(
+                      width: 400,
+                      height: 305,
+                      child: Form(
+                        key: _formKey,
                   child: Column(
                     children: [
-                      Text(
+                            const Text(
                         'Add Time Table',
                         style: TextStyle(fontSize: 28),
                       ),
-                      SizedBox(height: 20),
-                      MaterialTextFormField(hintText: "Timetable name"),
-                      SizedBox(height: 20),
+                            const SizedBox(height: 20),
+                            MaterialTextFormField(
+                              hintText: "Timetable name",
+                              controller: ref.read(timetableProvider).timetableNameController,
+                              validator: (value) => Validators.nameValidator(value),
+                            ),
+                            const SizedBox(height: 20),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           SizedBox(
-                            child: MaterialTextFormField(hintText: "7"),
-                            width: 100,
-                          ),
-                          Text('X'),
-                          SizedBox(
-                            child: MaterialTextFormField(hintText: ""),
-                            width: 100,
+                                  width: 70,
+                                  child: MaterialTextFormField(
+                                    controller: ref.read(timetableProvider).rowsController,
+                                    textAlign: TextAlign.center,
+                                    enabled: false,
+                                    hintText: '',
+                                  ),
+                                ),
+                                const SizedBox(width: 15),
+                                const Text('X', style: TextStyle(fontSize: 18)),
+                                const SizedBox(width: 15),
+                                Tooltip(
+                                  key: _toolTipKey,
+                                  verticalOffset: 40,
+                                  showDuration: const Duration(seconds: 1),
+                                  message: "Please enter number of columns",
+                                  triggerMode: TooltipTriggerMode.manual,
+                                  child: SizedBox(
+                                    width: 70,
+                                    child: MaterialTextFormField(
+                                      hintText: "",
+                                      textAlign: TextAlign.center,
+                                      controller: ref.read(timetableProvider).columnsController,
+                                      inputFormatters: [
+                                        FilteringTextInputFormatter.digitsOnly,
+                                        LengthLimitingTextInputFormatter(1),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 30),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Include Saturday?', style: TextStyle(fontSize: 15)),
+                                const SizedBox(width: 20),
+                                Consumer(
+                                  builder: (_, ref, __) => AnimatedToggleSwitch.dual(
+                                    height: 45,
+                                    spacing: 15,
+                                    onTap: (value) {
+                                      ref.read(timetableProvider.notifier).toggleIncludeSaturday();
+                                    },
+                                    current: ref.watch(timetableProvider).includeSaturday,
+                                    first: true,
+                                    second: false,
+                                    styleBuilder: (value) => value
+                                        ? ToggleStyle(
+                                            indicatorColor: Colors.teal,
+                                            backgroundColor: Colors.tealAccent.shade100,
+                                            borderColor: Colors.transparent,
+                                          )
+                                        : ToggleStyle(
+                                            indicatorColor: Colors.redAccent,
+                                            backgroundColor: Colors.redAccent.shade100,
+                                            borderColor: Colors.transparent,
+                                          ),
+                                    iconBuilder: (value) => value
+                                        ? const Icon(
+                                            Icons.check,
+                                            color: Colors.white,
+                                          )
+                                        : const Icon(
+                                            Icons.not_interested_rounded,
+                                            color: Colors.white,
+                                          ),
+                                  ),
                           ),
                         ],
                       ),
