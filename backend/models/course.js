a082b9b1a27ccf0e51141656c701c3c09e88f1d1
nailsonseat <aadarshv3@gmail.com>
2024-02-21 01:46:22 +0530

    update course schema

--- backend/models/course.js
+++ backend/models/course.js
@@ -1,33 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  courseCode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  primaryRoom: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  branches: [
+    {
+      type: String,
     },
-    courseCode: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    primaryRoom: {
-        type: String,
-    },
-    credits: {
-        type: Number,
-        required: true,
-    },
-    professorId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Faculty',
-        required: true,
-    },
-    branches: [{
-        type: String,
-        required: true
-    }]
+  ],
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 export default Course;
