79fefe9de8a759697268cb116ec9084306181d48
ashutosh229 <akumarjha875@gmail.com>
2025-03-03 03:57:26 +0530

    added the course resource route

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ import timetableListResource from "./resources/timetable/timetableListResource.j
 import timetableResource from "./resources/timetable/timetableResource.js";
 import messMenuListResource from "./resources/messMenu/messMenuListResource.js";
 import messMenuResource from "./resources/messMenu/messMenuResource.js";
+import courseResource from "./resources/faculty/courseResource.js";
 
 const PORT = `${process.env.PORT || 3000}`;
 const app = express();
@@ -53,8 +54,9 @@ app.use("/room", roomResource);
 app.use("/lost-and-found", lostAndFoundListResource);
 app.use("/lost-and-found-item", lostAndFoundResource);
 app.use("/messages", messageResource);
+app.use("/course", courseResource);
 
-app.get("/protected", tokenRequired, (req, res) => {
+app.get("/protected", tokenRequiredg, (req, res) => {
   res.json({ message: "Access granted" });
 });
 export default app;
