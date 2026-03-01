002037e96b5d897d0b045c89a972002d06a3a630
Amay Dixit <amaydixit11@gmail.com>
2026-03-02 00:00:00 +0530

    refactor: optimize internal logic in frontend/lib/components/material_textformfield.dart

--- frontend/lib/components/material_textformfield.dart
+++ frontend/lib/components/material_textformfield.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MaterialTextFormField extends StatelessWidget {
-  const MaterialTextFormField(
-      {super.key,
-      this.controller,
-      this.validator,
-      required this.hintText,
-      this.onChanged,
-      this.onSubmitted,
-      this.contentPadding,
-      this.hintColor,
-      this.enabled,
-      this.controllerLessValue,
-      this.onTap,
-      this.maxLength,
-      this.maxLines,
-      this.textAlign,
-      this.inputFormatters,});
+  const MaterialTextFormField({
+    super.key,
+    this.controller,
+    this.validator,
+    required this.hintText,
+    this.onChanged,
+    this.onSubmitted,
+    this.contentPadding,
+    this.hintColor,
+    this.enabled,
+    this.controllerLessValue,
+    this.onTap,
+    this.maxLength,
+    this.maxLines,
+    this.textAlign,
+    this.inputFormatters,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.obscureText = false,
+    this.autofocus = false,
+  });
 
   final TextEditingController? controller;
   final String? Function(String?)? validator;
@@ -33,13 +39,19 @@ class MaterialTextFormField extends StatelessWidget {
   final Function? onTap;
   final int? maxLength;
   final int? maxLines;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final bool obscureText;
+  final bool autofocus;
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     TextEditingController substituteController = TextEditingController();
     if (controllerLessValue != null) {
       substituteController.text = controllerLessValue!;
     }
+
     return TextFormField(
       onTap: () => onTap != null ? onTap!() : null,
       enabled: enabled ?? true,
@@ -47,31 +59,29 @@ class MaterialTextFormField extends StatelessWidget {
       textAlign: textAlign ?? TextAlign.start,
       maxLines: maxLines ?? 1,
       maxLength: maxLength,
+      autofocus: autofocus,
       inputFormatters: inputFormatters,
+      obscureText: obscureText,
       onChanged: (value) => onChanged != null ? onChanged!(value) : null,
       validator: (value) => validator != null ? validator!(value) : null,
-      onFieldSubmitted: (value) => onSubmitted != null ? onSubmitted!(value) : null,
-      cursorColor: Colors.teal.shade900,
-      style: TextStyle(
-        color: Colors.teal.shade900,
+      onFieldSubmitted: (value) =>
+          onSubmitted != null ? onSubmitted!(value) : null,
+      cursorColor: theme.colorScheme.primary,
+      style: GoogleFonts.inter(
+        color: theme.textTheme.bodyLarge?.color,
         fontSize: 15,
-        fontFamily: "RobotoFlex",
-        decoration: TextDecoration.none,
       ),
       decoration: InputDecoration(
-        contentPadding: contentPadding ?? const EdgeInsets.all(20),
+        contentPadding: contentPadding,
         hintText: hintText,
-        filled: true,
-        hintStyle: TextStyle(
-          color: hintColor ?? Colors.teal.shade900,
+        hintStyle: GoogleFonts.inter(
+          color: hintColor ??
+              theme.textTheme.bodyMedium?.color
+                  ?.withAlpha(128), // Using withAlpha for 0.5 opacity
           fontSize: 15,
-          fontFamily: "RobotoFlex",
         ),
-        fillColor: Colors.tealAccent.withOpacity(0.4),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide.none,
-        ),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
       ),
     );
   }
