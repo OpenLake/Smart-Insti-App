a1d3279406e4bd42921c1f924c15abab8d6c2234
Amay Dixit <amaydixit11@gmail.com>
2026-03-02 02:00:00 +0530

    refactor: optimize internal logic in frontend/lib/components/multiple_choice_selector.dart

--- frontend/lib/components/multiple_choice_selector.dart
+++ frontend/lib/components/multiple_choice_selector.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:search_choices/search_choices.dart';
 
-
-
-
 class MultipleChoiceSelector extends StatelessWidget {
   const MultipleChoiceSelector({super.key});
 
