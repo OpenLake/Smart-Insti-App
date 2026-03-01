3e474c255da2fb182fd8d98e09dc6381b293a136
Amay Dixit <amaydixit11@gmail.com>
2026-03-02 04:00:00 +0530

    refactor: optimize internal logic in frontend/lib/components/rounded_chip.dart

--- frontend/lib/components/rounded_chip.dart
+++ frontend/lib/components/rounded_chip.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class RoundedChip extends StatelessWidget {
-  const RoundedChip({super.key, required this.label, required this.onDeleted, required this.color});
+  const RoundedChip(
+      {super.key,
+      required this.label,
+      required this.onDeleted,
+      required this.color});
 
   final String label;
   final Function() onDeleted;
@@ -10,12 +14,14 @@ class RoundedChip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: ThemeData(splashFactory: InkRipple.splashFactory,splashColor: color.withOpacity(0.5)),
+      data: ThemeData(
+          splashFactory: InkRipple.splashFactory,
+          splashColor: color.withValues(alpha: 0.5)),
       child: Chip(
         label: Text(label),
         onDeleted: () => onDeleted(),
         side: BorderSide.none,
-        color: MaterialStateProperty.all<Color>(color),
+        color: WidgetStateProperty.all<Color>(color),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
           side: BorderSide.none,
