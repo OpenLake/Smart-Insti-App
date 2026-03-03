c0b1d1cfd8a53905bdd1b235ecdbaca8c14cf80c
Amay Dixit <amaydixit11@gmail.com>
2026-03-04 02:00:00 +0530

    refactor: optimize internal logic in frontend/lib/repositories/confession_repository.dart

--- frontend/lib/repositories/confession_repository.dart
+++ frontend/lib/repositories/confession_repository.dart
@@ -4,7 +4,8 @@ import 'package:logger/logger.dart';
 import '../constants/constants.dart';
 import '../models/confession.dart';
 
-final confessionRepositoryProvider = Provider<ConfessionRepository>((ref) => ConfessionRepository());
+final confessionRepositoryProvider =
+    Provider<ConfessionRepository>((ref) => ConfessionRepository());
 
 class ConfessionRepository {
   final Dio _client = Dio(
@@ -18,7 +19,8 @@ class ConfessionRepository {
   Future<List<Confession>> getConfessions(String token, {int page = 1}) async {
     _client.options.headers['authorization'] = 'Bearer $token';
     try {
-      final response = await _client.get('/confessions', queryParameters: {'page': page});
+      final response =
+          await _client.get('/confessions', queryParameters: {'page': page});
       if (response.data['status'] == true) {
         final List<dynamic> data = response.data['data'];
         return data.map((e) => Confession.fromJson(e)).toList();
@@ -31,13 +33,12 @@ class ConfessionRepository {
     }
   }
 
-  Future<bool> createConfession(String content, String backgroundColor, String token) async {
+  Future<bool> createConfession(
+      String content, String backgroundColor, String token) async {
     _client.options.headers['authorization'] = 'Bearer $token';
     try {
-      final response = await _client.post('/confessions', data: {
-        'content': content,
-        'backgroundColor': backgroundColor
-      });
+      final response = await _client.post('/confessions',
+          data: {'content': content, 'backgroundColor': backgroundColor});
       return response.data['status'] == true;
     } catch (e) {
       _logger.e(e);
@@ -48,22 +49,22 @@ class ConfessionRepository {
   Future<bool> likeConfession(String id, String token) async {
     _client.options.headers['authorization'] = 'Bearer $token';
     try {
-        final response = await _client.put('/confessions/$id/like');
-        return response.data['status'] == true;
+      final response = await _client.put('/confessions/$id/like');
+      return response.data['status'] == true;
     } catch (e) {
-        _logger.e(e);
-        return false;
+      _logger.e(e);
+      return false;
     }
   }
 
   Future<bool> reportConfession(String id, String token) async {
     _client.options.headers['authorization'] = 'Bearer $token';
     try {
-        final response = await _client.post('/confessions/$id/report');
-        return response.data['status'] == true;
+      final response = await _client.post('/confessions/$id/report');
+      return response.data['status'] == true;
     } catch (e) {
-        _logger.e(e);
-        return false;
+      _logger.e(e);
+      return false;
     }
   }
 }
