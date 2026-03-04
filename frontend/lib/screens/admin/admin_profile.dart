8bde0d4e4f85c7d0ba1d2d83012f2376db7ac6b7
Amay Dixit <amaydixit11@gmail.com>
2026-03-05 00:00:00 +0530

    refactor: optimize internal logic in frontend/lib/screens/admin/admin_profile.dart

--- frontend/lib/screens/admin/admin_profile.dart
+++ frontend/lib/screens/admin/admin_profile.dart
@@ -13,8 +13,10 @@ class AdminProfile extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (ref.read(authProvider.notifier).tokenCheckProgress != LoadingState.progress) {
-        ref.read(authProvider.notifier).verifyAuthTokenExistence(context, AuthConstants.adminAuthLabel.toLowerCase());
+      if (ref.read(authProvider.notifier).tokenCheckProgress !=
+          LoadingState.progress) {
+        ref.read(authProvider.notifier).verifyAuthTokenExistence(
+            context, AuthConstants.adminAuthLabel.toLowerCase());
       }
     });
     return ResponsiveScaledBox(
@@ -22,21 +24,30 @@ class AdminProfile extends ConsumerWidget {
       child: Scaffold(
         body: Consumer(
           builder: (_, ref, __) {
-            if (ref.read(authProvider).currentUser == null) {
+            final authState = ref.watch(authProvider);
+            if (authState.currentUser == null ||
+                authState.currentUser is! Admin) {
               return const Center(child: CircularProgressIndicator());
             } else {
-              Admin admin = ref.read(authProvider).currentUser as Admin;
+              Admin admin = authState.currentUser as Admin;
               return Center(
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 40),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text('Admin Profile', style: TextStyle(fontSize: 35, fontFamily: 'Jost')),
+                      const Text('Admin Profile',
+                          style: TextStyle(fontSize: 35, fontFamily: 'Jost')),
                       const SizedBox(height: 20),
-                      MaterialTextFormField(hintText: "Admin Name", enabled: false, controllerLessValue: admin.name),
+                      MaterialTextFormField(
+                          hintText: "Admin Name",
+                          enabled: false,
+                          controllerLessValue: admin.name),
                       const SizedBox(height: 20),
-                      MaterialTextFormField(hintText: "Admin Email", enabled: false, controllerLessValue: admin.email),
+                      MaterialTextFormField(
+                          hintText: "Admin Email",
+                          enabled: false,
+                          controllerLessValue: admin.email),
                     ],
                   ),
                 ),
