c7d1aa422e3723e7abf74a0336c24e331cda6ed5
Amay Dixit <amaydixit11@gmail.com>
2026-03-07 11:30:00 +0530

    modularize academic routes

---
+++ frontend/lib/routes/academics_routes.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../screens/academics/academics_home.dart';
+import '../screens/academics/acad_courses_screen.dart';
+import '../screens/academics/acad_timetable_screen.dart';
+import '../screens/academics/acad_curriculum_screen.dart';
+import '../screens/academics/acad_departments_screen.dart';
+
+final GoRoute academicsRoute = GoRoute(
+  path: 'academics',
+  pageBuilder: (context, state) => const MaterialPage(child: AcademicsHome()),
+  routes: [
+    GoRoute(
+      path: 'courses',
+      pageBuilder: (context, state) =>
+          const MaterialPage(child: AcadCoursesScreen()),
+    ),
+    GoRoute(
+      path: 'timetable',
+      pageBuilder: (context, state) =>
+          const MaterialPage(child: AcadTimetableScreen()),
+    ),
+    GoRoute(
+      path: 'curriculum',
+      pageBuilder: (context, state) =>
+          const MaterialPage(child: AcadCurriculumScreen()),
+    ),
+    GoRoute(
+      path: 'departments',
+      pageBuilder: (context, state) =>
+          const MaterialPage(child: AcadDepartmentsScreen()),
+    ),
+  ],
+);
